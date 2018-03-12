82aef7d3dbf6051a7f4f5bff156a38c750d8a21a
Nicole Thayer <nicole@carbonfive.com>
2018-03-12 14:45:28 -0700

    Added pug, sass, beginning to experiment with pug templating.

--- randomizer/routes/index.js
+++ randomizer/routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express yay', request: req, response: res, next: next });
+});
+
+/* GET home page. */
+router.get('/keytext', function(req, res, next) {
+  res.render('keytext', { title: 'Express yay', request: req, response: res, next: next });
 });
 
 module.exports = router;
+
+
+/*
+  IMMEDIATE PROJECTS
+
+
+
+*/
