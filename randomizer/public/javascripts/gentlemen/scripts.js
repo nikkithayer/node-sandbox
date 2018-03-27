4640b43a104add36c1d6b6a643f0928c26775914
Nicole Thayer <nicole@carbonfive.com>
2018-03-26 17:24:27 -0700

    WebGL experiment mostly working

--- randomizer/public/javascripts/gentlemen/scripts.js
+++ randomizer/public/javascripts/gentlemen/scripts.js
@@ -4,6 +4,15 @@ function setup() {
 
 
 function draw() {
+  background(0);
+  stroke(255);
+  strokeWeight(3);
+  var m = {x1: 0, y1: windowHeight, x2: 0, y2: windowHeight};
+  while(m.x2 < windowWidth){
+    line(m.x1, m.y1, m.x2, m.y2);
+    m.y1 -= 20;
+    m.x2 += 20;
+  }
   //if mouse over guy 1
   xOutMan();
   //if mouse over guy 2
--- randomizer/public/javascripts/threeD/scripts.js
+++ randomizer/public/javascripts/threeD/scripts.js
@@ -1,38 +1,90 @@
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  spaceMan = loadImage("images/threeD/spaceman.jpg");
+  img = loadImage('images/threeD/test.png');
 }
 
 function draw() {
-  background(0);
+  background(255);
   var locX = mouseX - width / 2;
   var locY = mouseY - height / 2;
-  // to set the light position,
-  // think of the world's coordinate as:
-  // -width/2,-height/2 -------- width/2,-height/2
-  //                |            |
-  //                |     0,0    |
-  //                |            |
-  // -width/2,height/2--------width/2,height/2
-  pointLight(255, locX, locY, 50);
-  specularMaterial(255);
+//  ambientLight(30);
+  ambientLight(100);
+  pointLight(255, 255,255, locX, locY, 50);
   noStroke();
-  rotateX(10);
-  texture(spaceMan);
-  //rotateX(frameCount * 0.01);
-  //rotateY(frameCount * 0.01);
-  var size = 100;
-  box(size);
-  for(i=0;i<6;i++){
-    translate(100,0,0);
-    size = size/1.25;
-    box(size);
-  }
-  size = 100;
-  translate(-600,0);
-  for(i=0;i<6;i++){
-    translate(0,100);
-    size = size/1.25;
-    box(size);
-  }
+
+if (mouseIsPressed) {
+//  rotateX(mouseX*0.1);
+  rotateY(mouseY*0.1);  
+//    translate(0,0,mouseY*0.1);
+  specularMaterial(120,255,255);
+}else{
+//  specularMaterial(20);  
+    specularMaterial(50,170,255); 
 }
+
+  createBox(72,400,94,0);
+  createSphere(41.5,100,420);
+  createBox(452,30,100,480);
+  createBox(225,40,171,309);
+  createCone(27,63,400,400,true);  
+  createBox(36,249,400,150);
+  createSphere(85.5,237,180);
+  createBox(238,145,214,0);  
+  //rotated --0.63!
+  createBox(450,24,551,425,-0.62);
+  //rotated!
+  createCone(135,207,504,100,false);
+  //rotated 0.6!
+  createBox(215,34,438,70,0.6);
+  createSphere(23.5,470,0);
+  createSphere(41,570,0);
+  createCone(201,469,730,0,true);
+  //rotated -0.55!
+  createBox(40,267,935,143,-0.55);
+  createSphere(58,1010,266);
+  createBox(150,150,1000,0);
+  createBox(76,102,1075,150)
+}
+
+function offsetWidth(num){
+  return (-windowWidth/2)+(num/2);
+}
+
+function offsetHeight(num){
+  return (windowHeight/2)-(num/2)-25;  
+}
+
+function createBox(width,height,x,y,rotation){
+  var depth = width;
+  push();
+    translate(offsetWidth(width)+x,offsetHeight(height)-y);
+    if(rotation!=undefined){
+      rotateZ(rotation);
+    }
+    box(width,height,depth);
+  pop();
+}
+
+function createSphere(radius,x,y){
+  push();
+    translate(offsetWidth(radius)+x,offsetHeight(radius)-y);
+    sphere(radius);
+  pop();  
+}
+
+function createCone(radius,height,x,y,rotation){
+  push();
+    translate(offsetWidth(radius)+x,offsetHeight(height)-y);
+    if(rotation==true){
+      rotateX(3);
+    }
+    cone(radius,height);
+  pop();    
+
+}
+
+//an array of objects with names
+//when you click on one it's selected
+//idk if it changes color, probably yes for development
+//it can spin in place when selected
+//normalize z-axis
